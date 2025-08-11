183f00e2999d11493bef76f03d0ce57879cf2715
Leficullen <lefirizqy2007@gmail.com>
2025-08-12 06:55:13 +0700

    feat: tambah logo navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,16 +15,22 @@ export default function Navbar() {
 
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
-      <div className="container mx-auto flex items-center justify-between p-4">
+      <div className="container mx-auto flex items-center justify-between px-4 py-2">
         {/* Logo dan Nama Aplikasi */}
-        <Link href="/" className="flex items-center gap-2">
-          <Image
-            src="/next.svg"
-            alt="Next.js logo"
-            width={80}
-            height={16}
+        <Link href="/" className="flex items-center">
+          <div className="w-fit shadow-sm rounded-2xl ">
+            <Image
+              src="/gatotkota-logo.svg"
+              alt="Next.js logo"
+              width={80}
+              height={16}
+              className="max-h-14"
           />
-          <span className="font-bold text-lg">ProyekUji</span>
+          </div>
+          <div className="flex flex-col ">
+            <span className="font-bold text-xl px-2">Gatot Kota</span>
+            <span className="font-bold text-xs px-2 text-zinc-500">Anda Lapor, Kami Meluncur</span>
+          </div>
         </Link>
 
         {/* Menu Navigasi */}
