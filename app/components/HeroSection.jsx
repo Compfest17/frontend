9d0cfdb68175f478274c3690f04f70cfa2535eec
Leficullen <lefirizqy2007@gmail.com>
2025-08-11 20:10:37 +0700

    test2

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,8 +2,9 @@ import Link from 'next/link';
 
 export default function HeroSection() {
   return (
-    <section className="text-center py-16 sm:py-20">
-      <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight mb-4">
+    <section className=" flex flex-col bg-cover bg-center h-screen items-center justify-center text-center py-auto sm:py-20" style={{backgroundImage:"url('/hero-img.svg')"}}>
+
+      <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight mb-4 flex ">
         Selamat Datang di GatotKota
       </h1>
       <p className="max-w-2xl mx-auto text-lg text-gray-600 mb-8">
